2fe29976fc29e2ef0aa0d7f3a70b91b297489ddb
Will Ballard <wballard@mailframe.net>
2013-02-10 16:26:46 -0600

    added ability to post messages

--- Makefile
+++ Makefile
@@ -4,11 +4,14 @@ install:
 	./setup.py install
 
 test: install
+	hipchat -i rooms list HipchatCLI | xargs -I _ hipchat rooms delete _
 	hipchat users show $(USERNAME)
 	hipchat -i users show $(USERNAME)
 	hipchat rooms create $(USERNAME) HipchatCLITest
 	hipchat rooms create $(USERNAME) HipchatCLITest2
 	hipchat rooms list
 	hipchat rooms list Test2
+	echo 'hi there' | hipchat rooms message HipchatCLITest $(USERNAME)
+	exit 1
 	hipchat -i rooms list HipchatCLI | xargs -I _ hipchat rooms delete _
 	hipchat rooms list
--- hipchatcli/cli.py
+++ hipchatcli/cli.py
@@ -5,6 +5,10 @@ Usage:
     hipchat [options] rooms create USER NAME
     hipchat [options] rooms list [PATTERN]
     hipchat [options] rooms delete ID
+    hipchat [options] rooms message ROOM USER
+
+Commands:
+    rooms message   This will read the message body from stdin by default.
 
 
 Options:
@@ -17,7 +21,8 @@ Parameters:
     PATTERN         Python style regex matches against all available fields
     ID              Integer ID used inside Hipchat for various objects
     USER            Integer ID or email address of a user
-    NAME            String used to name an object.
+    NAME            String used to name an object
+    ROOM            Name or integer ID of a chat room
 """
 
 from docopt import docopt
@@ -31,11 +36,26 @@ from clint.textui import colored
 from clint.textui import puts, indent, columns
 import re
 
+def input_content():
+    if select.select([sys.stdin,],[],[],0.0)[0]:
+        return sys.stdin.read()
+
 def forward(scope, arguments):
     for key in arguments.keys():
         if key in scope and arguments[key]:
             scope[key](arguments)
     return scope
+
+def api_key(arguments):
+    ret =  os.getenv('HIPCHAT_API_KEY', '')
+    assert ret, 'You must export HIPCHAT_API_KEY'
+    return ret
+
+def user(arguments, stripped=False):
+    if stripped:
+        return arguments['USER'].split('@')[0][0:15]
+    else:
+        return arguments['USER']
 
 def handle_error(response):
     """
@@ -56,7 +76,7 @@ def users(arguments):
 
     def show(arguments, silent=False):
         r = requests.get('https://api.hipchat.com/v1/users/show?format=json&auth_token={0}&user_id={1}'.
-                format(os.getenv('HIPCHAT_API_KEY', ''), arguments['USER']))
+                format(api_key(arguments), arguments['USER']))
         r = handle_error(r)
         if not silent:
             if arguments['--id-only']:
@@ -75,7 +95,7 @@ def users(arguments):
 def rooms(arguments):
 
     def list(arguments):
-        r = requests.get('https://api.hipchat.com/v1/rooms/list?format=json&auth_token={0}'.format(os.getenv('HIPCHAT_API_KEY', '')))
+        r = requests.get('https://api.hipchat.com/v1/rooms/list?format=json&auth_token={0}'.format(api_key(arguments)))
         r = handle_error(r)
         pattern = re.compile(arguments['PATTERN'] or '.*', re.IGNORECASE)
 
@@ -92,7 +112,7 @@ def rooms(arguments):
 
     def delete(arguments):
         r = requests.post(
-            'https://api.hipchat.com/v1/rooms/delete?format=json&auth_token={0}'.format(os.getenv('HIPCHAT_API_KEY', '')),
+            'https://api.hipchat.com/v1/rooms/delete?format=json&auth_token={0}'.format(api_key(arguments)),
             data={'room_id': arguments['ID']}
             )
         r = handle_error(r)
@@ -101,8 +121,19 @@ def rooms(arguments):
         #chain along to get the owner
         user = users(arguments)['show'](arguments, silent=True)
         r = requests.post(
-            'https://api.hipchat.com/v1/rooms/create?format=json&auth_token={0}'.format(os.getenv('HIPCHAT_API_KEY', '')),
+            'https://api.hipchat.com/v1/rooms/create?format=json&auth_token={0}'.format(api_key(arguments)),
             data={'owner_user_id': user['user_id'], 'name': arguments['NAME']}
+            )
+        r = handle_error(r)
+
+    def message(arguments):
+        r = requests.post(
+            'https://api.hipchat.com/v1/rooms/message?format=json&auth_token={0}'.format(api_key(arguments)),
+            data={
+                'room_id': arguments['ROOM'],
+                'from': user(arguments, stripped=True),
+                'message': input_content()
+            }
             )
         r = handle_error(r)
 
@@ -111,5 +142,4 @@ def rooms(arguments):
 
 def main():
     arguments = docopt(__doc__, version="{0} {1}".format(__package_name__, __package_version__))
-    #if select.select([sys.stdin,],[],[],0.0)[0]:
     forward(globals(), arguments)
