3eb478245bf0cdeef2c88421545a50ec0a4f3720
Will Ballard <wballard@mailframe.net>
2013-02-10 12:57:09 -0600

    added room create/delete along with the needed user show

--- Makefile
+++ Makefile
@@ -4,6 +4,11 @@ install:
 	./setup.py install
 
 test: install
+	hipchat users show $(USERNAME)
+	hipchat -i users show $(USERNAME)
+	hipchat rooms create $(USERNAME) HipchatCLITest
+	hipchat rooms create $(USERNAME) HipchatCLITest2
 	hipchat rooms list
-	hipchat rooms list Med
-	hipchat -i rooms list
+	hipchat rooms list Test2
+	hipchat -i rooms list HipchatCLI | xargs -I _ hipchat rooms delete _
+	hipchat rooms list
--- hipchatcli/cli.py
+++ hipchatcli/cli.py
@@ -1,10 +1,11 @@
 """Hipchat CLI
 
 Usage:
+    hipchat [options] users show USER
+    hipchat [options] rooms create USER NAME
     hipchat [options] rooms list [PATTERN]
+    hipchat [options] rooms delete ID
 
-Commands:
-    rooms list      List all available rooms, with an optional pattern on name
 
 Options:
     -i --id-only    Return just the ID, useful to pipe along with xargs
@@ -14,6 +15,9 @@ to authenticate. Set it before doing anything else.
 
 Parameters:
     PATTERN         Python style regex matches against all available fields
+    ID              Integer ID used inside Hipchat for various objects
+    USER            Integer ID or email address of a user
+    NAME            String used to name an object.
 """
 
 from docopt import docopt
@@ -26,6 +30,12 @@ import json
 from clint.textui import colored
 from clint.textui import puts, indent, columns
 import re
+
+def forward(scope, arguments):
+    for key in arguments.keys():
+        if key in scope and arguments[key]:
+            scope[key](arguments)
+    return scope
 
 def handle_error(response):
     """
@@ -42,10 +52,28 @@ def handle_error(response):
         puts(data['error']['message'])
         sys.exit(1)
 
+def users(arguments):
+
+    def show(arguments):
+        r = requests.get('https://api.hipchat.com/v1/users/show?format=json&auth_token={0}&user_id={1}'.
+                format(os.getenv('HIPCHAT_API_KEY', ''), arguments['USER']))
+        r = handle_error(r)
+        if arguments['--id-only']:
+            puts(str(r['user']['user_id']))
+        else:
+            puts(columns(
+                [str(r['user']['user_id']), 10],
+                [str(r['user']['name']), 25],
+                [str(r['user']['email']), 25],
+                [str(r['user']['title']), 10],
+                ))
+        return r['user']
+
+    return forward(locals(), arguments)
 
 def rooms(arguments):
 
-    def list():
+    def list(arguments):
         r = requests.get('https://api.hipchat.com/v1/rooms/list?format=json&auth_token={0}'.format(os.getenv('HIPCHAT_API_KEY', '')))
         r = handle_error(r)
         pattern = re.compile(arguments['PATTERN'] or '.*', re.IGNORECASE)
@@ -56,20 +84,31 @@ def rooms(arguments):
                     puts(str(room['room_id']))
                 else:
                     puts(columns(
-                        [str(room['room_id']), 9],
+                        [str(room['room_id']), 10],
                         [room['name'], 40],
                         [room['topic'], None]
                         ))
 
+    def delete(arguments):
+        r = requests.post(
+            'https://api.hipchat.com/v1/rooms/delete?format=json&auth_token={0}'.format(os.getenv('HIPCHAT_API_KEY', '')),
+            data={'room_id': arguments['ID']}
+            )
+        r = handle_error(r)
 
+    def create(arguments):
+        #chain along to get the owner
+        user = users(arguments)['show'](arguments)
+        r = requests.post(
+            'https://api.hipchat.com/v1/rooms/create?format=json&auth_token={0}'.format(os.getenv('HIPCHAT_API_KEY', '')),
+            data={'owner_user_id': user['user_id'], 'name': arguments['NAME']}
+            )
+        r = handle_error(r)
 
-    for key in arguments.keys():
-        if key in locals() and arguments[key]:
-            locals()[key]()
+    return forward(locals(), arguments)
+
 
 def main():
     arguments = docopt(__doc__, version="{0} {1}".format(__package_name__, __package_version__))
     #if select.select([sys.stdin,],[],[],0.0)[0]:
-    for key in arguments.keys():
-        if key in globals() and arguments[key]:
-            globals()[key](arguments)
+    forward(globals(), arguments)
